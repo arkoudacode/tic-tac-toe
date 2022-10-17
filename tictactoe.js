cebc7b81d6745cba56334ff053c6bbcd3913b6a2
Arkouda <arkoudacode@proton.me>
2022-10-16 22:48:22 -0500

    added first phase of logic building

---
+++ tictactoe.js
@@ -0,0 +1,27 @@
+// Tic-Tac-Toe Game
+// Path: game.html
+// Path: style.css
+
+function newGame() {
+    // TODO: start new game
+}
+
+function playerOne() {
+    // TODO: player one's turn
+}
+
+function playerTwo() {
+    // TODO: player two's turn
+}
+
+function checkWinner() {
+    // TODO: check for winner
+}
+
+function resetGame() {
+    // TODO: reset game
+}
+
+function playAgain() {
+    // TODO: play again
+}
